f1437a45625da29f51b94ee63aa9e78ef0b6bbe5
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-26 13:22:22 +0700

    Update Latihan23.java

--- src/latihan23/Latihan23.java
+++ src/latihan23/Latihan23.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
  Nama :Ma'rifatu Khirzah
  NIM :22205025
  Prodi :Teknik Informatika
+ Deskripsi :Menampilakn Program nilai terbesar dan terkecil yang inputannya berasal dari user.
  */
 public class Latihan23 {
 
